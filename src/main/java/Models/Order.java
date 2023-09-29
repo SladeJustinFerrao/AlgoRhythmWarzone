1532cea949f752c763922ebf3103e6d87a3aa715
Harshil Prajapati <94240178+hARSHILp7@users.noreply.github.com>
2023-09-29 10:01:33 -0400

    Update Order.java
    Variables added to class order in models package

--- src/main/java/Models/Order.java
+++ src/main/java/Models/Order.java
@@ -1,4 +1,24 @@
 package Models;
 
 public class Order {
+    /**
+     * Action of order
+     */
+    String d_ActionOfOrder;
+    /**
+     * Name of the targeted country
+     */
+    String d_CountryTargeted;
+    /**
+     * Name of the Source Country
+     */
+    String d_CountryOfSource;
+    /**
+     * Number of armies to be place
+     */
+    Integer d_ArmiesToPlace;
+    /**
+     * Object of the order class
+     */
+    Order OrderObj;
 }
