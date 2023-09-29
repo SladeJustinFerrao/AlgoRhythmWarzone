b95cfb9c1aa1d34a13783ef6dc727ca35874d5c0
KansaraDarshan <62127039+KansaraDarshan@users.noreply.github.com>
2023-09-28 20:58:05 -0400

    Update Player.java with doc comments

--- src/main/java/Models/Player.java
+++ src/main/java/Models/Player.java
@@ -42,6 +42,12 @@ public class Player {
     Integer d_noOfUnallocatedArmies;
 
 
+    /**
+	 * constructor has param used to create player with name and be deafault
+	 * armies.
+	 * 
+	 * @param p_playerName player name.
+	 */
     public Player(String p_playerName) {
         this.d_name = p_playerName;
         this.d_noOfUnallocatedArmies = 0;
@@ -49,70 +55,137 @@ public class Player {
     }
 
 
+	/**
+	 * This is no arg constructor.
+	 */
     public Player() {
 
     }
 
-
+    /**
+	 * This getter is used to get player's name.
+	 *
+	 * @return return player name.
+	 */
     public String getPlayerName() {
         return d_name;
     }
 
-
+  /**
+	 * This setter is used to set player's p_name.
+	 *
+	 * @param p_name set player name.
+	 */
     public void setPlayerName(String p_name) {
         this.d_name = p_name;
     }
 
 
+    /**
+	 * This getter is used to get color code for player.
+	 *
+	 * @return Color
+	 */
     public String getD_color() {
         return d_color;
     }
 
-
+  /**
+	 *
+	 * @param p_color color string.
+	 */
     public void setD_color(String p_color) {
         d_color = p_color;
     }
 
 
+    /**
+	 * This getter is used to get list of countries owned by player.
+	 *
+	 * @return return countries owned by player.
+	 */
     public List<Country> getD_coutriesOwned() {
         return d_coutriesOwned;
     }
 
+
+    /**
+	 * This setter is used to set list of countries owned by player.
+	 *
+	 * @param p_coutriesOwned set countries owned by player.
+	 */
     public void setD_coutriesOwned(List<Country> p_coutriesOwned) {
         this.d_coutriesOwned = p_coutriesOwned;
     }
 
 
+    /**
+	 * This getter is used to get list of continents owned by player.
+	 *
+	 * @return return list of continents owned by player.
+	 */
     public List<Continent> getD_continentsOwned() {
         return d_continentsOwned;
     }
 
 
+    /**
+	 * This setter is used to set list of continents owned by player.
+	 *
+	 * @param p_continentsOwned set continents owned by player.
+	 */
     public void setD_continentsOwned(List<Continent> p_continentsOwned) {
         this.d_continentsOwned = p_continentsOwned;
     }
 
-
+  /**
+	 * This getter is used to get execute orders of player.
+	 * 
+	 * @return return execute orders.
+	 */
     public List<Order> getD_ordersToExecute() {
         return d_ordersToExecute;
     }
 
 
+    /**
+	 * This setter is used to set execute orders player.
+	 * 
+	 * @param p_ordersToExecute set execute orders.
+	 */
     public void setD_ordersToExecute(List<Order> p_ordersToExecute) {
         this.d_ordersToExecute = p_ordersToExecute;
     }
 
+
+    /**
+	 * This getter is used to get allocated armies of player.
+	 * 
+	 * @return return allocated armies of player.
+	 */
 
     public Integer getD_noOfUnallocatedArmies() {
         return d_noOfUnallocatedArmies;
     }
 
 
+
+    /**
+	 * This setter is used to set number of allocated armies to player.
+	 * 
+	 * @param p_numberOfArmies set number of armies to player.
+	 */
     public void setD_noOfUnallocatedArmies(Integer p_numberOfArmies) {
         this.d_noOfUnallocatedArmies = p_numberOfArmies;
     }
 
 
+
+    /**
+	 * Extracts the list of names of countries owned by the player.
+	 *
+	 * @return list of country names
+	 */
     public List<String> getCountryNames(){
         List<String> l_countryNames=new ArrayList<String>();
         for(Country c: d_coutriesOwned){
@@ -122,6 +195,12 @@ public class Player {
     }
 
 
+
+    /**
+	 * Retrieves the list of continent names owned by the player.
+	 *
+	 * @return list of continent names
+	 */
     public List<String> getContinentNames(){
         List<String> l_continentNames = new ArrayList<String>();
         if (d_continentsOwned != null) {
@@ -134,6 +213,13 @@ public class Player {
     }
 
 
+    
+	/**
+	 * Issue order which takes order as an input and add it to players unassigned
+	 * orders pool.
+	 * 
+	 * @throws IOException exception in reading inputs from user
+	 */
     public void issue_order() throws IOException {
         BufferedReader l_reader = new BufferedReader(new InputStreamReader(System.in));
         PlayerService l_playerService = new PlayerService();
@@ -150,6 +236,12 @@ public class Player {
     }
 
 
+    /**
+	 * Gives the first order in the players list of orders, then removes it from the
+	 * list.
+	 * 
+	 * @return Order first order from the list of player's order
+	 */
     public Order next_order() {
         if (this.d_ordersToExecute==null||this.d_ordersToExecute.isEmpty()) {
             return null;
