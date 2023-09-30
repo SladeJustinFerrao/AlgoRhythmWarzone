9c5af65ce67cdf7e8ab28c20181dd540583eb37f
Harshil Prajapati <94240178+hARSHILp7@users.noreply.github.com>
2023-09-29 22:45:33 -0400

    Update Order.java
    Adding unparameterized and parameterized constructor and getter, setter methods to order class

--- src/main/java/Models/Order.java
+++ src/main/java/Models/Order.java
@@ -21,4 +21,52 @@ public class Order {
      * Object of the order class
      */
     Order OrderObj;
+
+    public Order() {
+    }
+    
+    public Order(String p_ActionOfOrder, String p_CountryTargeted, Integer p_ArmiesToPlace) {
+        this.d_ActionOfOrder = p_ActionOfOrder;
+        this.d_CountryTargeted = p_CountryTargeted;
+        this.d_ArmiesToPlace = p_ArmiesToPlace;
+    }
+    
+    public String getD_ActionOfOrder() {
+        return d_ActionOfOrder;
+    }
+    
+    public void setD_ActionOfOrder(String p_ActionOfOrder) {
+        this.d_ActionOfOrder = p_ActionOfOrder;
+
+    }
+    
+    public String getD_CountriesTargeted() {
+        return d_CountryTargeted;
+
+    }
+    
+    public void setD_CountriesTargeted(String p_CountriesTargeted) {
+        this.d_CountryTargeted = p_CountriesTargeted;
+
+    }
+    
+    public String getD_CountryOfSource() {
+        return d_CountryOfSource;
+
+    }
+    
+    public void setD_CountryOfSource(String p_CountryOfSource) {
+        this.d_CountryOfSource = p_CountryOfSource;
+
+    }
+    
+    public Integer getD_ArmiesToPlace() {
+        return d_ArmiesToPlace;
+
+    }
+    
+    public void setD_ArmiesToPlace(Integer p_ArmiesToPlace) {
+        this.d_ArmiesToPlace = p_ArmiesToPlace;
+
+    }
 }
