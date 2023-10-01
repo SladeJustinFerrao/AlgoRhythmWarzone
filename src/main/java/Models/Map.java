4ba466f23819d8c8a57d5ff1e04e074700191685
arjunyadav <arjunyadav15597@gmail.com>
2023-10-01 15:45:16 -0400

    mapfile getter and setter

--- src/main/java/Models/Map.java
+++ src/main/java/Models/Map.java
@@ -1,4 +1,28 @@
 package Models;
 
+import java.util.List;
+
 public class Map {
+    /**
+     * stores the map file name.
+     */
+    String d_mapFile;
+
+    /**
+     * getter method for map file.
+     *
+     * @return d_mapfile
+     */
+    public String getD_mapFile() {
+        return d_mapFile;
+    }
+
+    /**
+     * setter method for map file.
+     *
+     * @param p_mapFile file name of map
+     */
+    public void setD_mapFile(String p_mapFile) {
+        this.d_mapFile = p_mapFile;
+    }
 }
