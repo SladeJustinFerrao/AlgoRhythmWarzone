b13f6a460f8a7446190f64d12feab5ef76795538
SladeJustinFerrao <sladejustincredible@gmail.com>
2023-10-01 15:53:27 -0400

    created List of continents and countries and created getter and setter functions for both

--- src/main/java/Models/Map.java
+++ src/main/java/Models/Map.java
@@ -1,10 +1,22 @@
 package Models;
 
+import java.util.List;
+
 public class Map {
     /**
      * stores the map file name.
      */
     String d_mapFile;
+
+    /**
+     * List of Continents
+     */
+    List<Continent> d_continents;
+
+    /**
+     * List of Countries
+     */
+    List<Country> d_countries;
 
     /**
      * getter method for map file.
@@ -22,5 +34,41 @@ public class Map {
      */
     public void setD_mapFile(String p_mapFile) {
         this.d_mapFile = p_mapFile;
+    }
+
+    /**
+     * Getter method to get list of continents
+     *
+     * @return List of continents
+     */
+    public List<Continent> getD_continents() {
+        return d_continents;
+    }
+
+    /**
+     * Setter method to set list of continents
+     *
+     * @param d_continents List of continents
+     */
+    public void setD_continents(List<Continent> d_continents) {
+        this.d_continents = d_continents;
+    }
+
+    /**
+     * Getter method to get the list of countries
+     *
+     * @return List of countries
+     */
+    public List<Country> getD_countries() {
+        return d_countries;
+    }
+
+    /**
+     * Setter method to set the list of countries
+     *
+     * @param d_countries List of countries
+     */
+    public void setD_countries(List<Country> d_countries) {
+        this.d_countries = d_countries;
     }
 }
