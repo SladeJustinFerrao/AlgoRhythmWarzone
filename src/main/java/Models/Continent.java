2dfcdda8a0aae4547ed3b76c8a47e1f93fa8e955
SladeJustinFerrao <133801711+SladeJustinFerrao@users.noreply.github.com>
2023-10-01 13:02:22 -0400

    Update Continent.java
    Added getters and setters for continent class

--- src/main/java/Models/Continent.java
+++ src/main/java/Models/Continent.java
@@ -51,4 +51,93 @@ public class Continent {
     public Continent(String d_continentName) {
         this.d_continentName = d_continentName;
     }
+    /**
+     * Getter method to get continent ID
+     *
+     * @return Continent ID
+     */
+    public Integer getD_continentID(){
+        return d_continentID;
+    }
+
+    /**
+     * Setter method to set continent ID
+     *
+     * @param p_continentID Continent ID
+     */
+    public void setD_continentID(int p_continentID) {
+        this.d_continentID = p_continentID;
+    }
+
+    /**
+     * Getter method to get continent name
+     *
+     * @return Continent Name
+     */
+    public String getD_continentName() {
+        return d_continentName;
+    }
+
+    /**
+     * Setter method to set continent name
+     *
+     * @param p_continentName Continent name
+     */
+    public void setD_continentName(String p_continentName) {
+        this.d_continentName = p_continentName;
+    }
+
+    /**
+     * Getter method to get the continent value
+     *
+     * @return Continent value
+     */
+    public Integer getD_continentValue() {
+        return d_continentValue;
+    }
+
+    /**
+     * Setter method to set the continent value
+     *
+     * @param p_continentValue Continent value
+     */
+    public void setD_continentValue(int p_continentValue) {
+        this.d_continentValue = p_continentValue;
+    }
+
+    /**
+     * Getter method to get the list of countries in the continent
+     *
+     * @return List of Countries
+     */
+    public List<Integer> getD_countries() {
+        return d_countries;
+    }
+
+    /**
+     * Setter method to set the List of countries in the continent
+     *
+     * @param p_countries List of Countries
+     */
+    public void setD_countries(List<Integer> p_countries) {
+        this.d_countries = p_countries;
+    }
+
+
+    public void addCountry(int p_country) {
+        if(d_countries!=null) {
+            d_countries.add(p_country);
+        } else {
+            d_countries = new ArrayList<Integer>();
+            d_countries.add(p_country);
+        }
+    }
+
+    public void removeCountries (int p_country) {
+        if(d_countries==null) {
+            System.out.println("There are no countries to remove");
+        } else {
+            d_countries.remove(p_country);
+        }
+    }
 }
