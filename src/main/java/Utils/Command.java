2846e93711726e8f760790e7129661e8ded6e39f
Beavan1997 <mathiasbeavan@gmail.com>
2023-10-01 20:03:22 -0400

    Updating Command class to handle clubbed commands

--- src/main/java/Utils/Command.java
+++ src/main/java/Utils/Command.java
@@ -31,16 +31,23 @@ public class Command {
 
     /**
      * Retrieves the Tasks to be performed under the Main Activity.
-     * @return Map of Operations and Arguments for the command
+     * @return List of Map of Operations and Arguments for the command
      */
-    public Map<String,String> getTaskandArguments(){
+    public List<Map<String , String>>  getTaskandArguments(){
         String l_baseCommand = getMainCommand();
         String l_tasks = d_command.replace(l_baseCommand,"").trim();
+        List<Map<String , String>> l_taskList  = new ArrayList<Map<String,String>>();
+
+        if(l_tasks.charAt(0)!='-'){
+            l_tasks = "-filename "+l_tasks;
+        }
 
         Map<String,String> l_tasksMap = new HashMap<String,String>();
-
-        l_tasksMap = getIndividualTasksandArguments(l_tasks);
-        return l_tasksMap;
+        String[] l_taskString = l_tasks.split("-");
+        for (int i=1;i<l_taskString.length;i++) {
+            l_taskList.add(getIndividualTasksandArguments(l_taskString[i]));
+        }
+        return l_taskList;
     }
 
     /**
@@ -53,7 +60,7 @@ public class Command {
         Map<String, String> l_taskMap = new HashMap<String, String>();
         String l_arguments = "";
 
-        l_taskMap.put("operation", l_taskList[0].replace("-",""));
+        l_taskMap.put("operation", l_taskList[0]);
 
         if(l_taskList.length > 1){
             l_arguments = p_tasks.replace(l_taskList[0],"").trim();
