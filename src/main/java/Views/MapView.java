52444958639a9c7e0b589d6984ae99a48ff0da0b
hARSHILp7 <prajapatiharshil3107@gmail.com>
2023-10-02 13:23:15 -0400

    Adding variables in MapView class

--- src/main/java/Views/MapView.java
+++ src/main/java/Views/MapView.java
@@ -1,7 +1,35 @@
 package Views;
 
+import Models.Player;
+import Models.GameState;
+import Models.Map;
+import Models.Country;
+import Models.Continent;
+
+import java.util.List;
+
 /**
  * MapView class for Views
  */
 public class MapView {
+    /**
+     * List of players
+     */
+    List<Player> d_players;
+    /**
+     * Current state of game
+     */
+    GameState d_gameState;
+    /**
+     * Current map
+     */
+    Map d_map;
+    /**
+     * List of countries
+     */
+    List<Country> d_countries;
+    /**
+     * List of continents
+     */
+    List<Continent> d_continents;
 }
