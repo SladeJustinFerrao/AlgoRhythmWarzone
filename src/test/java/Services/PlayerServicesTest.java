086c93ee3d778013e2288fca62daa3dc9e01fd65
KansaraDarshan <kansaradarshan100@gmail.com>
2023-10-02 12:20:05 -0400

    test cases init added for player services

---
+++ src/test/java/Services/PlayerServicesTest.java
@@ -0,0 +1,56 @@
+package Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerServicesTest {
+
+    @Test
+    void isPlayerNameUnique() {
+    }
+
+    @Test
+    void addRemovePlayers() {
+    }
+
+    @Test
+    void checkPlayersAvailability() {
+    }
+
+    @Test
+    void assignCountries() {
+    }
+
+    @Test
+    void createDeployOrder() {
+    }
+
+    @Test
+    void validateDeployOrderArmies() {
+    }
+
+    @Test
+    void calculateArmiesForPlayer() {
+    }
+
+    @Test
+    void assignArmies() {
+    }
+
+    @Test
+    void unexecutedOrdersExists() {
+    }
+
+    @Test
+    void unassignedArmiesExists() {
+    }
+
+    @Test
+    void updatePlayers() {
+    }
+
+    @Test
+    void isMapLoaded() {
+    }
+}
