58d03eacce10ce41ba25f97687026a85750b37d0
arjunyadav <arjunyadav15597@gmail.com>
2023-10-04 14:57:27 -0400

    added removeNeighbourFromCountry test cases

---
+++ src/test/java/Models/CountryTest.java
@@ -0,0 +1,24 @@
+package Models;
+
+import Services.MapService;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountryTest {
+
+    @Test
+    void removeNeighbourFromCountry() {
+        MapService l_serivce = new MapService();
+        GameState l_game = new GameState();
+        Map l_map = l_serivce.loadMap(l_game, l_serivce.getFilePath("canada.map"));
+        List<Country> l_countries = l_map.getD_countries();
+        int l_initCount = l_countries.get(0).getD_neighbourCountryId().size();
+        l_countries.get(0).removeNeighbourFromCountry(8);
+        int l_finalCount = l_countries.get(0).getD_neighbourCountryId().size();
+        assertEquals(l_initCount-1,l_finalCount);
+    }
+
+}
