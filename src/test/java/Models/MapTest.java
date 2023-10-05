4ff3c81fe5efd8677cc7bbf415272e2f63ad792b
SladeJustinFerrao <sladejustincredible@gmail.com>
2023-10-04 20:43:30 -0400

    added test for validation of map

--- src/test/java/Models/MapTest.java
+++ src/test/java/Models/MapTest.java
@@ -9,6 +9,43 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MapTest {
 
+    /**
+     * Invalid Test to see if all elements of the map are connected
+     */
+    @Test
+    void invalidMapTest() {
+
+        GameEngine l_gameEngine = new GameEngine();
+        GameState d_gameState = new GameState();
+        boolean l_isExcep = false;
+        try {
+            Command l_command = new Command("loadmap TestInvalidMap.map");
+            l_gameEngine.performLoadMap(l_command);
+        } catch (Exception l_e){
+            l_isExcep=true;
+        }
+
+        assertTrue(l_isExcep);
+    }
+
+    /**
+     * Valid test to see if all elements of map are connectedS
+     */
+    @Test
+    void validMapTest() {
+
+        GameEngine l_gameEngine = new GameEngine();
+        GameState d_gameState = new GameState();
+        boolean l_isExcep = false;
+        try {
+            Command l_command = new Command("loadmap canada.map");
+            l_gameEngine.performLoadMap(l_command);
+        } catch (Exception l_e){
+            l_isExcep=true;
+        }
+        assertFalse(l_isExcep);
+    }
+
     @Test
     void isCountriesConnectedTest() {
         MapService l_serivce = new MapService();
@@ -17,6 +54,7 @@ class MapTest {
 
         assertEquals(true,l_map.isCountriesConnected());
     }
+
     @Test
     void isContinentsConnectedTest() {
         MapService l_serivce = new MapService();
@@ -25,5 +63,4 @@ class MapTest {
 
         assertEquals(true,l_map.isContinentsConnected());
     }
-
 }
