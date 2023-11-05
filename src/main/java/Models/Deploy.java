d5b4e7246acc4887901314ec11a4277df3e816ff
hARSHILp7 <prajapatiharshil3107@gmail.com>
2023-11-05 18:45:22 -0500

    Comments added in deploy class

--- src/main/java/Models/Deploy.java
+++ src/main/java/Models/Deploy.java
@@ -6,17 +6,43 @@ import Constants.GameConstants;
  * Concrete command of command pattern
  */
 public class Deploy implements Order {
+    /**
+     * Name of target country
+     */
     String d_targetCountryName;
+
+    /**
+     * Number of armies to place
+     */
     Integer d_numberOfArmiesToPlace;
+
+    /**
+     * Player Initiator
+     */
     Player d_playerInitiator;
+
+    /**
+     * Sets the Log containing information about orders
+     */
     String d_orderExecutionLog;
 
+    /**
+     * The constructor receives all the parameters necessary to implement the order
+     * These are then encapsulated in the order
+     * @param p_playerInitiator player that created the order
+     * @param p_targetCountryName country name that will receive new armies
+     * @param p_numberOfArmiesToPlace number of armies to place
+     */
     public Deploy(Player p_playerInitiator, String p_targetCountryName, Integer p_numberOfArmiesToPlace) {
         this.d_targetCountryName = p_targetCountryName;
         this.d_playerInitiator = p_playerInitiator;
         this.d_numberOfArmiesToPlace = p_numberOfArmiesToPlace;
     }
 
+    /**
+     * Executes the deployed order
+     * @param p_gameState current game state
+     */
     @Override
     public void execute(GameState p_gameState) {
         if(valid(p_gameState)) {
@@ -37,6 +63,11 @@ public class Deploy implements Order {
         p_gameState.updateLog(orderExecutionLog(), GameConstants.OUTCOME);
     }
 
+    /**
+     * Checks whether target country belongs to the player or not
+     * @param p_gameState current game state
+     * @return true if country belongs to the player or else false
+     */
     @Override
     public boolean valid(GameState p_gameState) {
         Country l_country = d_playerInitiator.getD_coutriesOwned().stream()
@@ -45,17 +76,29 @@ public class Deploy implements Order {
         return l_country != null;
     }
 
+    /**
+     * Prints the deploy order
+     */
     @Override
     public void printOrder() {
         this.d_orderExecutionLog = "\n---------- Deploy order issued by player " + this.d_playerInitiator.getPlayerName() + " ----------\n" + System.lineSeparator() + "Deploy " + this.d_numberOfArmiesToPlace + " armies to " + this.d_targetCountryName;
         System.out.println(this.d_orderExecutionLog);
     }
 
+    /**
+     * Gets order execution log
+     * @return order execution log
+     */
     @Override
     public String orderExecutionLog() {
         return d_orderExecutionLog;
     }
 
+    /**
+     * Prints and sets order execution log
+     * @param p_orderExecutionLog string to set as log
+     * @param p_logType type of log : error, default
+     */
     public void setD_orderExecutionLog(String p_orderExecutionLog, String p_logType) {
         this.d_orderExecutionLog = p_orderExecutionLog;
         if(p_logType.equals("error")) {
@@ -66,6 +109,10 @@ public class Deploy implements Order {
         }
     }
 
+    /**
+     * Get order name
+     * @return String which contains order name
+     */
     @Override
     public String getOrderName() {
         return "deploy";
