f81eadc63f0aebdc6f866b2e888744e51ed23abf
Beavan1997 <mathiasbeavan@gmail.com>
2023-11-04 22:33:13 -0400

    Updating LogBuffer Class

--- src/main/java/Models/LogEntryBuffer.java
+++ src/main/java/Models/LogEntryBuffer.java
@@ -1,5 +1,7 @@
 package Models;
 
+import Views.LogWriter;
+import Constants.GameConstants;
 import java.util.Observable;
 
 /**
@@ -7,4 +9,50 @@ import java.util.Observable;
  */
 public class LogEntryBuffer extends Observable {
 
+    String d_logInput;
+
+    /**
+     * Constructor
+     */
+    public LogEntryBuffer(){
+        LogWriter l_logWriter = new LogWriter();
+        this.addObserver(l_logWriter);
+    }
+
+    /**
+     * Getter for the Log Input.
+     * @return Log Input.
+     */
+    public String getD_logMessage(){
+        return d_logInput;
+    }
+
+    /**
+     * Sets the log value and category.
+     * @param p_log Log Input Value
+     * @param p_category Category of the log.
+     */
+    public void currentLog(String p_log, String p_category){
+
+        switch(p_category.toLowerCase()){
+            case GameConstants.COMMAND:
+                d_logInput =GameConstants.COMMANDLOG + p_log;
+                break;
+            case GameConstants.ORDER:
+                d_logInput =GameConstants.ORDERLOG + p_log;
+                break;
+            case GameConstants.PHASE:
+                d_logInput = GameConstants.SEP + p_log + GameConstants.SEP;
+                break;
+            case GameConstants.OUTCOME:
+                d_logInput = GameConstants.LOG + p_log;
+                break;
+            case GameConstants.STARTLOG:
+            case GameConstants.ENDLOG:
+                d_logInput = p_log;
+                break;
+        }
+        setChanged();
+        notifyObservers();
+    }
 }
---
+++ src/main/java/Views/LogWriter.java
@@ -0,0 +1,15 @@
+package Views;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Class to Write the logs to the file.
+ */
+public class LogWriter implements Observer {
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+}
