a12351c10096c98955ace279638330c44f751ead
arjunyadav <arjunyadav15597@gmail.com>
2023-11-06 16:20:15 -0500

    added performSaveMap and performMapEdit functionality

--- src/main/java/Models/StartUpPhase.java
+++ src/main/java/Models/StartUpPhase.java
@@ -2,8 +2,11 @@ package Models;
 
 import Controller.GameEngine;
 import Utils.Command;
+import Utils.UncaughtExceptionHandler;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class StartUpPhase extends Phase {
 
@@ -35,22 +38,6 @@ public class StartUpPhase extends Phase {
     protected void performLoadMap(Command p_command, Player p_player) throws IOException {
 
     }
-
-    @Override
-    protected void performSaveMap(Command p_command, Player p_player) throws IOException {
-
-    }
-
-    @Override
-    protected void performMapEdit(Command p_command, Player p_player) throws IOException {
-
-    }
-
-    @Override
-    protected void performEditContinent(Command p_command, Player p_player) throws IOException {
-
-    }
-
     @Override
     protected void createPlayers(Command p_command, Player p_player) throws IOException {
 
@@ -85,5 +72,62 @@ public class StartUpPhase extends Phase {
     public void initPhase() {
 
     }
+    /**
+     * {@inheritDoc}
+     */
+
+    public void performMapEdit(Command p_command, Player p_player) throws IOException {
+        List<java.util.Map<String, String>> l_operations_list = p_command.getTaskandArguments();
+
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler(d_gameState));
+
+        if (l_operations_list == null || l_operations_list.isEmpty()) {
+            System.out.println("Invalid Command");
+        } else {
+            for (java.util.Map<String, String> l_map : l_operations_list) {
+                if (p_command.checkRequiredKeysPresent("arguments", l_map)) {
+                    d_mapService.editMap(d_gameState, l_map.get("arguments"));
+                } else {
+                    System.out.println("Invalid Command");
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void performEditContinent(Command p_command, Player p_player) throws IOException {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void performSaveMap(Command p_command, Player p_player) {
+        if (!l_isMapLoaded) {
+            d_gameEngine.setD_gameEngineLog("No map found to save, Please `editmap` first", "effect");
+            return;
+        }
+
+        List<Map<String, String>> l_operations_list = p_command.getTaskandArguments();
+
+        Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler(d_gameState));
+        if (null == l_operations_list || l_operations_list.isEmpty()) {
+            System.out.println("Invalid Command");
+        } else {
+            for (Map<String, String> l_map : l_operations_list) {
+                if (p_command.checkRequiredKeysPresent("arguments", l_map)) {
+                    boolean l_fileUpdateStatus = d_mapService.saveMap(d_gameState,
+                            l_map.get("arguments"));
+                    if (l_fileUpdateStatus) {
+                        d_gameEngine.setD_gameEngineLog("Required changes have been made in map file", "effect");
+                    } else
+                        System.out.println(d_gameState.getError());
+                } else {
+                    System.out.println("Invalid Command");
+                }
+            }
+        }
+    }
+
 
 }
