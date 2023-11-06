68204143965b3bba7bfff3866f789cd06f1eefc0
SladeJustinFerrao <sladejustincredible@gmail.com>
2023-11-05 21:02:53 -0500

    added command handler function to handle the player commands

--- src/main/java/Models/Phase.java
+++ src/main/java/Models/Phase.java
@@ -1,5 +1,42 @@
 package Models;
 
-public class Phase {
+import Controller.GameEngine;
+import Utils.Command;
+
+import java.io.IOException;
+
+public abstract class Phase {
+
+    /**
+     * Stores the information about the current GamePlay.
+     */
+    GameState d_gameState;
+
+    /**
+     * Stores the information about the current GamePlay.
+     */
+    GameEngine d_gameEngine;
+
+    /**
+     * Flag to determine if the map is loaded.
+     */
+    boolean l_isMapLoaded;
+
+    /**
+     * Redirect to specific phase implementations the command which were entered  by user.
+     *
+     * @param p_enteredCommand Command entered by the user in the Command Line Interface (CLI)
+     * @param p_player         Player instance
+     * @throws IOException Indicates a failure in I/O operation
+     */
+    private void commandHandler(String p_enteredCommand, Player p_player) throws IOException {
+        Command l_command = new Command(p_enteredCommand);
+        String l_rootCommand = l_command.getMainCommand();
+        l_isMapLoaded = d_gameState.getD_map() != null;
+
+        d_gameState.updateLog(l_command.getD_command(), "command");
+
+    }
+
 
 }
