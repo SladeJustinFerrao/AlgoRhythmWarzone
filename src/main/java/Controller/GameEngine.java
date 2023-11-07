3ae558c0405af250e96845afd2ea3c54c9466570
YugKotak <yugkotak9745@gmail.com>
2023-11-06 23:14:11 -0500

    GameEngine restructuring

--- src/main/java/Controller/GameEngine.java
+++ src/main/java/Controller/GameEngine.java
@@ -1,6 +1,8 @@
 package Controller;
 
 import Models.GameState;
+import Models.Phase;
+import Models.StartUpPhase;
 
 /**
  * This is the entry point of the Game and keeps the track of current Game State.
@@ -11,4 +13,11 @@ public class GameEngine {
 	 * d_gameState stores the information about current GamePlay.
 	 */
 	public GameState d_gameState = new GameState();
+
+	Phase d_currentPhase = new StartUpPhase(this, d_gameState);
+
+	private void setD_CurrentPhase(Phase p_phase){
+		d_currentPhase = p_phase;
+	}
+
 }
