81711b0a0144b4c54b7f057e126b6dbb636c7c79
arjunyadav <arjunyadav15597@gmail.com>
2023-11-07 21:56:03 -0500

    added performCreateDeploy ,performShowMap function

--- src/main/java/Models/OrderExecutionPhase.java
+++ src/main/java/Models/OrderExecutionPhase.java
@@ -22,12 +22,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>validatemap</strong> command for checking
-     * required arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command Command entered by the user on CLI
-     * @param p_player  Instance of Player Object
-     * @throws Exception Indicates a failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performValidateMap(Command p_command, Player p_player) throws Exception {
@@ -35,12 +30,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>editneighbor</strong> command for checking
-     * required arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command Command entered by the user in the CLI
-     * @param p_player  Instance of Player Object
-     * @throws Exception Handles File I/O Exception
+     * {@inheritDoc}
      */
     @Override
     protected void performEditNeighbour(Command p_command, Player p_player) throws Exception {
@@ -48,12 +38,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>editcountry</strong> command for checking
-     * required arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command Command entered by the user on CLI
-     * @param p_player  Instance of Player Object
-     * @throws Exception Handles File I/O Exception
+     * {@inheritDoc}
      */
     @Override
     protected void performEditCountry(Command p_command, Player p_player) throws Exception {
@@ -61,12 +46,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>loadmap</strong> command for checking required
-     * arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command command entered by the user on CLI
-     * @param p_player  instance of Player Object
-     * @throws Exception indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performLoadMap(Command p_command, Player p_player) throws Exception {
@@ -74,12 +54,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>savemap</strong> command for checking required
-     * arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command command entered by the user on CLI
-     * @param p_player  instance of Player Object
-     * @throws Exception indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performSaveMap(Command p_command, Player p_player) throws Exception {
@@ -87,12 +62,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>editmap</strong> command for checking required
-     * arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command command entered by the user on CLI
-     * @param p_player  instance of Player Object
-     * @throws Exception indicates when failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performMapEdit(Command p_command, Player p_player) throws Exception {
@@ -100,12 +70,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>editcontinent</strong> command for checking
-     * required arguments and redirecting control to the model for actual processing.
-     *
-     * @param p_command command entered by the user on CLI
-     * @param p_player  instance of Player Object
-     * @throws Exception indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performEditContinent(Command p_command, Player p_player) throws Exception {
@@ -113,11 +78,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of gameplayer command for checking required arguments and redirecting control to the model for adding or removing players.
-     *
-     * @param p_command Command entered by the user in the CLI
-     * @param p_player  Instance of Player Object
-     * @throws Exception Indicates a failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void createPlayers(Command p_command, Player p_player) throws Exception {
@@ -125,23 +86,16 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Handles the deployment order in the gameplay.
-     *
-     * @param p_command Command entered by the user
-     * @param p_player  Instance of the player object
-     * @throws Exception Indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performCreateDeploy(String p_command, Player p_player) throws Exception {
+        printInvalidCommandInState();
 
     }
 
     /**
-     * Handles the advance order in the gameplay.
-     *
-     * @param p_command Command entered by the user
-     * @param p_player  Instance of the player object
-     * @throws Exception Indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performAdvance(String p_command, Player p_player) throws Exception {
@@ -150,11 +104,7 @@ public class OrderExecutionPhase extends Phase {
 
 
     /**
-     * Handles the card commands.
-     *
-     * @param p_enteredCommand String representing the entered command
-     * @param p_player         Player instance
-     * @throws Exception Signals an I/O exception
+     * {@inheritDoc}
      */
     @Override
     protected void performCardHandle(String p_enteredCommand, Player p_player) throws Exception {
@@ -162,12 +112,7 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Basic validation of <strong>assigncountries</strong> to check for required
-     * arguments and redirect control to the model for assigning countries to players.
-     *
-     * @param p_command Command entered by the user in the CLI
-     * @param p_player  Instance of the Player Object
-     * @throws Exception Indicates failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performAssignCountries(Command p_command, Player p_player) throws Exception {
@@ -175,14 +120,12 @@ public class OrderExecutionPhase extends Phase {
     }
 
     /**
-     * Handles the 'show map' command.
-     *
-     * @param p_command Command entered by the user
-     * @param p_player  Player object instance
-     * @throws Exception Indicates a failure in I/O operation
+     * {@inheritDoc}
      */
     @Override
     protected void performShowMap(Command p_command, Player p_player) throws Exception {
+        MapView l_mapView = new MapView(d_gameState);
+        l_mapView.showMap();
 
     }
 
@@ -244,7 +187,7 @@ public class OrderExecutionPhase extends Phase {
 
 
     /**
-     * This method signifies the main functionality executed on phase change.
+     * {@inheritDoc}
      */
     @Override
     public void initPhase() {
