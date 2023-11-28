8bebfe2905bbc28e9d1eacb038952418cbf88f5a
arjunyadav <arjunyadav15597@gmail.com>
2023-11-27 21:27:50 -0500

    added comments and Javadoc for cheaterPlayer class

--- src/main/java/Models/CheaterPlayer.java
+++ src/main/java/Models/CheaterPlayer.java
@@ -6,9 +6,21 @@ import java.util.Random;
 
 import Services.PlayerServices;
 
+/**
+ * The CheaterPlayer class represents a player in a game who employs a cheating strategy during the issue order phase.
+ * This strategy involves directly attacking neighboring enemy countries and doubling the number of armies on the player's
+ * own countries that have enemy neighbors.
+ */
 public class CheaterPlayer extends PlayerBehavior {
 
-
+    /**
+     * Creates a new order for the specified player based on the given player and game state.
+     *
+     * @param p_player The Player object for which the order is being generated.
+     * @param p_gameState The GameState object representing the current state of the game.
+     *
+     * @return An Order object representing the newly generated order for the specified player.
+     */
     @Override
     public String createOrder(Player p_player, GameState p_gameState) throws IOException {
 
@@ -35,6 +47,14 @@ public class CheaterPlayer extends PlayerBehavior {
         p_player.checkForMoreOrders(true);
         return null;
     }
+
+    /**
+     * Doubles the number of armies on countries that are neighbors to enemy countries for the specified player,
+     * based on the given player and game state.
+     *
+     * @param p_player The Player object for whom the armies on neighboring countries are being doubled.
+     * @param p_gameState The GameState object representing the current state of the game.
+     */
     private void doubleArmyOnEnemyNeighboredCounties(Player p_player, GameState p_gameState){
         List<Country> l_countriesOwned = p_player.getD_coutriesOwned();
 
@@ -57,10 +77,25 @@ public class CheaterPlayer extends PlayerBehavior {
 
         }
     }
+
+    /**
+     * Returns a random country owned by the player from the provided list of countries.
+     *
+     * @param p_listOfCountries List of countries owned by the player.
+     * @return A random country from the list of owned countries.
+     */
     private Country getRandomCountry(List<Country> p_listOfCountries){
         Random l_random = new Random();
         return p_listOfCountries.get(l_random.nextInt(p_listOfCountries.size()));
     }
+
+    /**
+     * Conquers all enemy countries that are neighbors to the countries owned by the player,
+     * based on the given player and game state.
+     *
+     * @param p_player The Player object initiating the conquest.
+     * @param p_gameState The GameState object representing the current state of the game.
+     */
     private void conquerNeighboringEnemies(Player p_player, GameState p_gameState){
         List<Country> l_countriesOwned = p_player.getD_coutriesOwned();
 
@@ -82,6 +117,16 @@ public class CheaterPlayer extends PlayerBehavior {
         }
 
     }
+
+    /**
+     * Initiates the conquest of the target country when it doesn't have any armies, transferring ownership
+     * from the target country's player to the cheater player.
+     *
+     * @param p_gameState       The current state of the game.
+     * @param p_cheaterPlayer   The player owning the source country.
+     * @param p_targetCPlayer   The player owning the target country.
+     * @param p_targetCountry   The target country of the battle.
+     */
     private void conquerTargetCountry(GameState p_gameState, Player p_targetCPlayer, Player p_cheaterPlayer, Country p_targetCountry) {
         p_targetCPlayer.getD_coutriesOwned().remove(p_targetCountry);
         p_targetCPlayer.getD_coutriesOwned().add(p_targetCountry);
@@ -89,6 +134,14 @@ public class CheaterPlayer extends PlayerBehavior {
         this.updateContinents(p_cheaterPlayer, p_targetCPlayer, p_gameState);
     }
 
+    /**
+     * Updates the continents of players based on the results of a battle, specifically involving the cheater player
+     * (owner of the source country), the target country's player, and the current state of the game.
+     *
+     * @param p_cheaterPlayer   The player owning the source country.
+     * @param p_targetCPlayer   The player owning the target country.
+     * @param p_gameState       The current state of the game.
+     */
     private void updateContinents(Player p_cheaterPlayer, Player p_targetCPlayer,
                                   GameState p_gameState) {
         List<Player> l_playesList = new ArrayList<>();
@@ -100,6 +153,14 @@ public class CheaterPlayer extends PlayerBehavior {
         PlayerServices l_playerService = new PlayerServices();
         l_playerService.performContinentAssignment(l_playesList, p_gameState.getD_map().getD_continents());
     }
+
+    /**
+     * Retrieves the owner of the country with the specified ID and searches for its neighbors in the current game state.
+     *
+     * @param p_gameState The current state of the game.
+     * @param p_countryId The ID of the country for which neighbors are to be searched.
+     * @return The owner of the specified country.
+     */
     private Player getCountryOwner(GameState p_gameState, Integer p_countryId){
         List<Player> l_players = p_gameState.getD_players();
         Player l_owner = null;
@@ -115,6 +176,12 @@ public class CheaterPlayer extends PlayerBehavior {
         return l_owner;
     }
 
+    /**
+     * give Enemies
+     * @param p_player
+     * @param p_country
+     * @return l_enemyNeighbors
+     */
     private ArrayList<Integer> getEnemies(Player p_player, Country p_country){
         ArrayList<Integer> l_enemyNeighbors = new ArrayList<Integer>();
 
@@ -125,24 +192,36 @@ public class CheaterPlayer extends PlayerBehavior {
         return l_enemyNeighbors;
     }
 
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String createDeployOrder(Player p_player, GameState p_gameState) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String createAdvanceOrder(Player p_player, GameState p_gameState) {
         return null;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String createCardOrder(Player p_player, GameState p_gameState, String p_cardName) {
         return null;
     }
 
+    /**
+     * This method returns the player behavior.
+     * @return player behavior String
+     */
     @Override
     public String getPlayerBehavior() {
-        return null;
+        return "Cheater";
     }
 }
