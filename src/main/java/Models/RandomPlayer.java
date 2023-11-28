885fe9b5c512688f31b3976a7b52a90d8a2d4945
YugKotak <yugkotak9745@gmail.com>
2023-11-27 23:14:23 -0500

    Random Player class

--- src/main/java/Models/RandomPlayer.java
+++ src/main/java/Models/RandomPlayer.java
@@ -2,6 +2,7 @@ package Models;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class RandomPlayer extends PlayerBehavior{
@@ -10,12 +11,53 @@ public class RandomPlayer extends PlayerBehavior{
 
     @Override
     public String getPlayerBehavior() {
-        return null;
+        return "Random";
     }
 
     @Override
     public String createOrder(Player p_player, GameState p_gameState) throws IOException {
-        return null;
+        String l_command;
+        if (!checkIfArmiesDeployed(p_player)) {
+            if(p_player.getD_noOfUnallocatedArmies()>0) {
+                l_command = createDeployOrder(p_player, p_gameState);
+            }else{
+                l_command = createAdvanceOrder(p_player, p_gameState);
+            }
+        } else {
+            if(p_player.getD_cardsOwnedByPlayer().size()>0){
+                int l_index = (int) (Math.random() * 3) +1;
+                switch (l_index) {
+                    case 1:
+                        l_command = createDeployOrder(p_player, p_gameState);
+                        break;
+                    case 2:
+                        l_command = createAdvanceOrder(p_player, p_gameState);
+                        break;
+                    case 3:
+                        if (p_player.getD_cardsOwnedByPlayer().size() == 1) {
+                            l_command = createCardOrder(p_player, p_gameState, p_player.getD_cardsOwnedByPlayer().get(0));
+                            break;
+                        } else {
+                            Random l_random = new Random();
+                            int l_randomIndex = l_random.nextInt(p_player.getD_cardsOwnedByPlayer().size());
+                            l_command = createCardOrder(p_player, p_gameState, p_player.getD_cardsOwnedByPlayer().get(l_randomIndex));
+                            break;
+                        }
+                    default:
+                        l_command = createAdvanceOrder(p_player, p_gameState);
+                        break;
+                }
+            } else{
+                Random l_random = new Random();
+                Boolean l_randomBoolean = l_random.nextBoolean();
+                if(l_randomBoolean){
+                    l_command = createDeployOrder(p_player, p_gameState);
+                }else{
+                    l_command = createAdvanceOrder(p_player, p_gameState);
+                }
+            }
+        }
+        return l_command;
     }
 
     @Override
