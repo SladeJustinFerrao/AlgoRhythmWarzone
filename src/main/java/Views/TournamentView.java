38aa0a6ae386e432b09ff44af0736fbd4f7ea9ed
SladeJustinFerrao <sladejustincredible@gmail.com>
2023-11-28 18:25:25 -0500

    Added constructor for TournamentView.java

--- src/main/java/Views/TournamentView.java
+++ src/main/java/Views/TournamentView.java
@@ -1,4 +1,17 @@
 package Views;
 
+import Models.GameState;
+import Models.Tournament;
+
+import java.util.List;
+
 public class TournamentView {
+    Tournament d_tournament;
+
+    List<GameState> d_gameStateObjects;
+
+    public TournamentView(Tournament p_tournament) {
+        d_tournament = p_tournament;
+        d_gameStateObjects = d_tournament.getD_gameStateList();
+    }
 }
