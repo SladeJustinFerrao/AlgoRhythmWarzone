81fc071aa6b2dedd663dc50ec2c445617a625d0d
Beavan1997 <mathiasbeavan@gmail.com>
2023-11-29 23:04:28 -0500

    Adding TestClass

--- src/test/java/Services/GamePlayServiceTest.java
+++ src/test/java/Services/GamePlayServiceTest.java
@@ -12,12 +12,21 @@ import static org.junit.jupiter.api.Assertions.*;
 class GamePlayServiceTest {
 
     @Test
-    void saveGame() throws Exception {
+    void saveLoadGame() throws Exception {
+
         GameState l_gameState = new GameState();
         GameEngine l_gameEngine = new GameEngine();
         Phase l_phase = new StartUpPhase(l_gameEngine,l_gameState);
-        l_phase.handleCommand("editmap canada.map");
-        Map l_map = l_phase.getD_gameState().getD_map();
-        assertEquals("canada.map", l_phase.getD_gameState().getD_map().getD_mapFile());
+        boolean l_isExcep = false;
+        try {
+            l_phase.handleCommand("loadmap canada.map");
+            l_phase.handleCommand("gameplayer -add A -add B");
+            GamePlayService.saveGame(l_phase, "check.txt");
+            GamePlayService.loadGame("check.txt");
+            l_phase.handleCommand("validatemap");
+        } catch (Exception l_e){
+            l_isExcep=true;
+        }
+        assertFalse(l_isExcep);
     }
 }
